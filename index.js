018f5fc73faeeb1ad2e2c4f17b945c2717429536
nirglus <nirgluskin@gmail.com>
2023-10-30 22:01:12 +0200

    add fetchAdvice func

---
+++ index.js
@@ -0,0 +1,12 @@
+const fetchAdvice = () => {
+    const url = `https://api.adviceslip.com/advice`;
+    fetch(url)
+       .then(res => res.json())
+       .then(data => {
+        console.log(data);
+        const adviceId = document.getElementById("adviceId");
+        const advice = document.getElementById("adivceTxt");
+        adviceId.innerHTML = `${data.slip.id}`
+        advice.innerHTML = `"${data.slip.advice}"`
+       })
+}
